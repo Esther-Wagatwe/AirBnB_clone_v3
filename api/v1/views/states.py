08539c6affa50793de2667023087f0742e26860e
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-09 23:34:46 +0300

    State

---
+++ api/v1/views/states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A module for viewing State objects."""
+from api.v1.views import app_views
+from flask import jsonify, abort
+from models.state import State
+from models import storage
+import uuid
+
+
+@app_views.route('/states')
+def statesList():
+    """Returns the list of all State objects"""
+    states_list = [obj.to_dict() for obj in storage.all("State").values()]
+    return jsonify(states_list)
+
+
+@app_views.route('/states/<state_id>')
+def stateID(state_id):
+    """Returns state object according to ID"""
+    states = storage.all("State").values()
+    state = [obj.to_dict() for obj in states if obj.id == state_id]
+    if state[0] is None:
+        abort(404)
+    return jsonify(state[0])
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """
+    Deletes a State Object
+    """
+
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    storage.delete(state)
+    storage.save()
+
+    return make_response(jsonify({}), 200)
