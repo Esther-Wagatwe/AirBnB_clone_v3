216f93220e44eed05da079f5b20ad604e076e88b
ngari-kev <kevngariwangui@gmail.com>
2024-06-10 19:00:18 +0300

    city works

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -41,3 +41,48 @@ def del_city(city_id):
     storage.save()
 
     return jsonify({}), 200
+
+
+@app_views.route('/states/<state_id>/cities/', methods=['POST'])
+def create_city(state_id):
+    """Creates a City object"""
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+
+    states = storage.all("State").values()
+    state = [obj.to_dict() for obj in states if obj.id == state_id]
+
+    if state == []:
+        abort(404)
+
+    cities = []
+    new_city = City(name=request.json['name'], state_id=state_id)
+    storage.new(new_city)
+    storage.save()
+    cities.append(new_city.to_dict())
+
+    return jsonify(cities[0]), 201
+
+
+@app_views.route('/cities/<city_id>', methods=['PUT'])
+def update_city(city_id):
+    """Updates a City object"""
+    cities = storage.all("City").values()
+    city = [obj.to_dict() for obj in cities if obj.id == city_id]
+
+    if city == []:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, 'Not a JSON')
+
+    city[0]['name'] = request.json['name']
+    for obj in cities:
+        if obj.id == city_id:
+            obj.name = request.json['name']
+    storage.save()
+
+    return jsonify(city[0]), 200
