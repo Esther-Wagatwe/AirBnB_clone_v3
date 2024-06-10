a15edd66e1880d29a31dcd8b3a2e45e1c6f1cee1
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-06-10 12:36:46 +0300

    Add POST & PUT methods

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from os import getenv
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A module for viewing State objects."""
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from models.state import State
 from models import storage
 
@@ -24,11 +24,8 @@ def stateID(state_id):
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
-def delete_state(state_id):
-    """
-    Deletes a State Object with a specific ID
-    """
-
+def state_delete(state_id):
+    """Deletes a State Object with a specific ID"""
     state = storage.get(State, state_id)
 
     if not state:
@@ -38,3 +35,36 @@ def delete_state(state_id):
     storage.save()
 
     return jsonify({}), 200
+
+
+@app_views.route('/states', methods=['POST'])
+def post():
+    """Performs a POST operation on State."""
+    state = []
+    req_data = request.get_json()
+    if not req_data:
+        abort(404, 'Not a JSON')
+    if 'name' not in req_data:
+        abort(400, 'Missing name')
+    new_state = State(name=req_data['name'])
+    storage.new(new_state)
+    storage.save()
+    state.append(new_state.to_dict())
+    return jsonify(state[0]), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def put(state_id):
+    """Perform a PUT operation on State."""
+    states = storage.all("State").values()
+    state = [obj.to_dict() for obj in states if obj.id == state_id]
+    if not state:
+        abort(404)
+    if not request.get_json():
+        abort(404, 'Not a JSON')
+    state[0]['name'] == request.json['name']
+    for obj in states:
+        if obj.id == state_id:
+            obj.name = request.json['name']
+    storage.save()
+    return jsonify(state[0]), 200
